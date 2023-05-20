1a88bb6ca3a739e2b72f662cad15e50688f78c29
Chi-kwan Chan <chanchikwan@gmail.com>
2023-05-20 01:25:52 -0700

    Implement `UnmarshalYAML()`

--- pkg/laniscope/laniscope.go
+++ pkg/laniscope/laniscope.go
@@ -22,3 +22,21 @@ type Spec struct {
 	Backend  backend.Backend `yaml:"backend,omitempty"`
 	raw      yaml.Node
 }
+
+func (s *Spec) UnmarshalYAML(value *yaml.Node) error {
+	var decoded struct {
+		Backend  backend.Backend `yaml:"backend,omitempty"`
+		Features []Laniscope     `yaml:"features,omitempty"`
+	}
+	if err := value.Decode(&decoded); err != nil {
+		return err
+	}
+	s.Backend  = decoded.Backend
+	s.Features = decoded.Features
+	s.raw      = *value
+	return nil
+}
+
+func (s Spec) Node() yaml.Node {
+	return s.raw
+}
