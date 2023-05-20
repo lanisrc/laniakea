d56c44ef83c338d7c45142b76b21317db347d329
Chi-kwan Chan <chanchikwan@gmail.com>
2023-05-20 02:50:42 -0700

    Additional validates for `Laniscope.spec`

--- pkg/laniscope/laniscope.go
+++ pkg/laniscope/laniscope.go
@@ -2,6 +2,7 @@ package laniscope
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 	"gopkg.in/yaml.v3"
 )
@@ -39,8 +40,24 @@ func (s Laniscope) Validate() error {
 	if s.Kind == "" {
 		return errors.New("kind is required")
 	}
+
 	if s.Metadata.Name == "" {
 		return errors.New("metadata.name is required")
 	}
+
+	if s.Spec.Kind == 0 || s.Spec.Tag == "!!null" {
+		return nil
+	}
+	if s.Spec.Kind != yaml.MappingNode {
+		return errors.New("spec must be a mapping")
+	}
+	// A yaml.Node bypasses the decoder's normal duplicate-key check.
+	// Decode a temporary view to verify the complete raw spec
+	// without interpreting fields owned by a particular kind.
+	var fields map[string]any
+	if err := s.Spec.Decode(&fields); err != nil {
+		return fmt.Errorf("spec: %w", err)
+	}
+
 	return nil
 }
