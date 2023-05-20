9391e1c98252c0d4a0e824dfd6fe718ce74a0699
Chi-kwan Chan <chanchikwan@gmail.com>
2023-05-20 01:18:30 -0700

    Describe package `backend`

---
+++ pkg/backend/doc.go
@@ -0,0 +1,8 @@
+// Package backend defines Laniscope backend selectors.
+//
+// A backend selector names the implementation that should realize a
+// Laniscope.
+// Backend-specific configuration belongs under spec.backend.spec in
+// the configuration YAML file.
+//
+package backend
