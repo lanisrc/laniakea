a97acb02e69899d1f8b47506f5bf2eebf84a27f9
Chi-kwan Chan <chanchikwan@gmail.com>
2023-05-20 02:31:48 -0700

    Ensure input files contain only one YAML document

--- pkg/laniscope/yaml.go
+++ pkg/laniscope/yaml.go
@@ -2,16 +2,26 @@ package laniscope
 
 import (
 	"bytes"
+	"errors"
+	"io"
 	"gopkg.in/yaml.v3"
 )
 
-// Decode decodes one Laniscope YAML document from data.
+// Decode decodes exactly one Laniscope YAML document from data.
 func Decode(data []byte) (Laniscope, error) {
 	var scope Laniscope
 	decoder := yaml.NewDecoder(bytes.NewReader(data))
 	decoder.KnownFields(true)
 
 	if err := decoder.Decode(&scope); err != nil {
+		return Laniscope{}, err
+	}
+
+	var extra yaml.Node
+	if err := decoder.Decode(&extra); err != io.EOF {
+		if err == nil {
+			return Laniscope{}, errors.New("multiple YAML documents are not supported")
+		}
 		return Laniscope{}, err
 	}
 
