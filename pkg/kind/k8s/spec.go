70d2dacaaf720ec144e7d10876087911c279f278
Chi-kwan Chan <chanchikwan@gmail.com>
2023-05-20 21:54:30 -0700

    Draft the Kubernetes kind

---
+++ pkg/kind/k8s/spec.go
@@ -0,0 +1,8 @@
+// Package k8s defines the Kubernetes interface.
+package k8s
+
+// Kind define the kind name
+const Kind = "Kubernetes"
+
+// Spec contains state shared by different backends.
+type Spec struct{}
